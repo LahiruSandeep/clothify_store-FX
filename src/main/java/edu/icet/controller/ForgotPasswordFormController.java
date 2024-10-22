0c24c779c8587686f9389f7bd4f29e4ebee8dfb0
Lahiru Sandeepa <lahirusandeepa9999@gmai.com>
2024-10-22 23:28:34 +0530

    second update

--- src/main/java/edu/icet/controller/ForgotPasswordFormController.java
+++ src/main/java/edu/icet/controller/ForgotPasswordFormController.java
@@ -1,4 +1,137 @@
 package edu.icet.controller;
 
-public class ForgotPasswordFormController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import edu.icet.util.CrudUtil;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ForgotPasswordFormController implements Initializable {
+
+    public JFXPasswordField txtPassword;
+    public JFXPasswordField txtConfirmPassword;
+    public JFXTextField txtEmail;
+    public JFXTextField txtOtp;
+    public JFXButton btnSaved;
+    public JFXButton btnOk;
+
+    StringProperty variable = new SimpleStringProperty("");
+    StringProperty variable2 = new SimpleStringProperty("");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        txtPassword.textProperty().bindBidirectional(variable);
+        txtConfirmPassword.textProperty().bindBidirectional(variable2);
+        AtomicReference<String> val1 = new AtomicReference<>("");
+        AtomicReference<String> val2 = new AtomicReference<>("");
+        variable.addListener((observable, oldValue, newValue) -> {
+            val1.set(newValue);
+            conformPassword(val1, val2);
+        });
+        variable2.addListener((observable, oldValue, newValue) -> {
+            val2.set(newValue);
+            conformPassword(val1, val2);
+        });
+
+    }
+
+    public void conformPassword(AtomicReference<String> newValueTxt1, AtomicReference<String> newValueTxt2) {
+        if (txtPassword.getText().equals("") && txtConfirmPassword.getText().equals("")) {
+            btnSaved.setDisable(true);
+        } else if (newValueTxt1.get().equals(newValueTxt2.get())) {
+            btnSaved.setDisable(false);
+        } else {
+            btnSaved.setDisable(true);
+        }
+    }
+
+    public void backBtnOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/login_form.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            // Get the current window
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            // Close the previous window
+            currentStage.close();
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void btnSentOtpOnAction(ActionEvent actionEvent) {
+
+    }
+
+    char[] otp;
+
+    public void btnOkOtpOnAction(ActionEvent actionEvent) {
+        String OTP = "";
+        for (char s : otp) {
+            OTP += s;
+        }
+        if (txtOtp.getText().equals(OTP)) {
+            new Alert(Alert.AlertType.INFORMATION, "OTP Matched :)").show();
+            txtPassword.setDisable(false);
+            txtConfirmPassword.setDisable(false);
+
+
+        } else {
+            txtPassword.setDisable(true);
+            txtConfirmPassword.setDisable(true);
+            btnSaved.setDisable(true);
+            new Alert(Alert.AlertType.ERROR, "invalid OTP :(").show();
+        }
+    }
+
+    public char[] getOtp(int len) {
+        String numbers = "0123456789";
+        Random rNd = new Random();
+        char[] otp = new char[len];
+        for (int i = 0; i < len; i++) {
+            otp[i] = numbers.charAt(rNd.nextInt(numbers.length()));
+
+        }
+        return otp;
+    }
+
+
+    public void sendEmail() {
+
+    }
+
+    public void btnSavedOnAction(ActionEvent actionEvent) {
+        try {
+            boolean isUpdate= CrudUtil.execute("UPDATE user\n" +
+                    "SET password = ?\n" +
+                    "WHERE email = ?;",txtConfirmPassword.getText(),txtEmail.getText());
+            if (isUpdate){
+                new Alert(Alert.AlertType.INFORMATION, "Your password change Success !").show();
+            }else {
+                new Alert(Alert.AlertType.ERROR, "Your password change Failed !").show();
+
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
--- src/main/java/edu/icet/controller/UserRegistrationFormController.java
+++ src/main/java/edu/icet/controller/UserRegistrationFormController.java
@@ -1,4 +1,161 @@
 package edu.icet.controller;
 
-public class UserRegistrationFormController {
+import com.jfoenix.controls.*;
+import edu.icet.util.CrudUtil;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class UserRegistrationFormController implements Initializable {
+    public JFXTextField txtAdminUserName;
+    public JFXTextField txtOtp;
+    public JFXTextField txtUserName;
+    public JFXComboBox redUserType;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtAdminPassword;
+    public JFXPasswordField txtUserPassword;
+    public JFXPasswordField txtConformUserPassword;
+    public JFXButton verifyBtn;
+    public JFXButton btnSend;
+    public JFXCheckBox checkBoxUserPassword;
+    public JFXCheckBox checkBoxAdminPassword;
+    public JFXComboBox cmbUserType;
+    public JFXButton btnCreate;
+    StringProperty variable = new SimpleStringProperty("");
+    StringProperty variable2 = new SimpleStringProperty("");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        txtUserPassword.textProperty().bindBidirectional(variable);
+        txtConformUserPassword.textProperty().bindBidirectional(variable2);
+        AtomicReference<String> val1= new AtomicReference<>("");
+        AtomicReference<String> val2= new AtomicReference<>("");
+        variable.addListener((observable, oldValue, newValue) -> {
+            val1.set(newValue);
+            conformPassword(val1,val2);
+        });
+        variable2.addListener((observable, oldValue, newValue) -> {
+            val2.set(newValue);
+            conformPassword(val1,val2);
+        });
+
+
+        cmbUserType.getItems().addAll("Admin", "User");
+
+    }
+    public void conformPassword(AtomicReference<String> newValueTxt1, AtomicReference<String> newValueTxt2) {
+        if(txtUserPassword.getText().equals("") && txtConformUserPassword.getText().equals("")){
+            btnCreate.setDisable(true);
+        }else if (newValueTxt1.get().equals(newValueTxt2.get())) {
+            btnCreate.setDisable(false);
+        }else{
+            btnCreate.setDisable(true);
+        }
+    }
+
+
+    public void backBtnOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/login_form.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void btnAdminCheckOnAction(ActionEvent actionEvent) {
+        try {
+            ResultSet resultSet = CrudUtil.execute("SELECT user_name, password FROM user WHERE user_type = 'Admin' AND user_name = ? AND password = ?",
+                    txtAdminUserName.getText(),
+                    txtAdminPassword.getText()
+            );
+            if (resultSet.next()) {
+                new Alert(Alert.AlertType.INFORMATION, "Admin user found.").show();
+                txtUserName.setDisable(false);
+                txtEmail.setDisable(false);
+                txtOtp.setDisable(false);
+                btnSend.setDisable(false);
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Admin user not found !").show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnCreateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnOtpOnAction(ActionEvent actionEvent) {
+        sendEmail();
+    }
+
+    public char[] getOtp(int len) {
+        String numbers = "0123456789";
+        Random rNd = new Random();
+        char[] otp = new char[len];
+        for (int i = 0; i < len; i++) {
+            otp[i] = numbers.charAt(rNd.nextInt(numbers.length()));
+
+        }
+        return otp;
+    }
+
+    char[] otp;
+
+    public void sendEmail() {
+
+    }
+
+    public void verifyBtnOnAction(ActionEvent actionEvent) {
+        String OTP = "";
+        for (char s : otp) {
+            OTP += s;
+        }
+        if (txtOtp.getText().equals(OTP)) {
+            new Alert(Alert.AlertType.INFORMATION, "OTP Matched :)").show();
+            cmbUserType.setDisable(false);
+            txtUserPassword.setDisable(false);
+            txtConformUserPassword.setDisable(false);
+
+        } else {
+            new Alert(Alert.AlertType.ERROR, "invalid OTP :(").show();
+        }
+    }
+
+    public void checkBoxUserPasswordOnAction(ActionEvent actionEvent) {
+        System.out.println("checkBoxUserPasswordOnAction");
+        checkBoxUserPassword.setVisible(true);
+    }
+
+    public void checkBoxAdminPasswordOnAction(ActionEvent actionEvent) {
+        if (checkBoxAdminPassword.isSelected()) {
+            System.out.println("selected");
+            txtAdminPassword.setText(txtAdminPassword.getText());
+
+        } else {
+            System.out.println("un selected");
+        }
+    }
 }
